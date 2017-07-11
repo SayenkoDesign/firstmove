8a701f295c67a4eb45bdca3c1367500c3a87adf4
Robert Parker <rparker@kodama.design>
2017-07-11 05:04:45 -0700

    sub menu nav

--- wp-content/themes/Supertheme/web/scripts/app.js
+++ wp-content/themes/Supertheme/web/scripts/app.js
@@ -2,6 +2,7 @@
 
 jQuery(function() {
     jQuery(document).foundation();
+
     jQuery('.sticky li.fancybox').each(function(){
         jQuery(this).removeClass('fancybox');
         var child = jQuery(this).children('a');
@@ -10,6 +11,7 @@ jQuery(function() {
         child.attr('href', 'javascript:;');
         child.attr('data-src', id);
     });
+
     jQuery('.fancybox').fancybox({
         toolbar : true
     });
@@ -24,4 +26,36 @@ jQuery(function() {
         e.preventDefault();
         return false;
     });
+
+    // scroll to
+    function getParameterByName(name, url) {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
+    jQuery('.sticky-container a').click(function(e){
+        var scrollto = getParameterByName('scrollTo', jQuery(this).attr("href"));
+        var href = jQuery(this).attr("href");
+        href = href.substring(0, href.indexOf('?'));
+        if(href == window.location.pathname) {
+            jQuery('html, body').animate({
+                scrollTop: jQuery('#' + scrollto).offset().top - 200
+            }, 2000);
+            if(jQuery('#small-menu').is(':visible')) {
+                jQuery('.sticky .menu-icon').click();
+            }
+            e.preventDefault();
+            return false;
+        }
+        return false;
+    });
+    if(getParameterByName('scrollTo')) {
+        jQuery('html, body').animate({
+            scrollTop: jQuery("#" + getParameterByName('scrollTo')).offset().top - 200
+        }, 2000);
+    }
 });
